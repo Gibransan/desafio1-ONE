bb13ebb8345aacc3fdc02cf823d25fe1dd6a5367
Gib <gibransan@outlook.com>
2023-05-23 17:45:19 -0600

    Script terminado y html de ejemplo en proceso

--- script.js
+++ script.js
@@ -1,33 +1,47 @@
 function reemplazarVocales() {
-    let originalText = document.getElementById("original-text").value;
-    let encriptedText = "";
+     let textoOriginal = document.getElementById("texto-original").value;
+     let textoReemplazado = "";
 
-    for (let i = 0; i < originalText.length; i++) {
-        let letra = originalText.chartAt(i);
-        let reemplazo = "";
-        
-        switch (letra) {
-            case 'a':
-                reemplazo = "ai";
-                break;
-            case 'e':
-                reemplazo = "enter";
-                break;
-            case 'i':
-                reemplazo = "imes";
-                break;
-            case 'o':
-                reemplazo = "ober";
-                break;
-            case 'u':
-                reemplazo = "ufat";
-                break;
-            default:
-                reemplazo = letra;
-        }
+     for (let i = 0; i < textoOriginal.length; i++) {
+          let letra = textoOriginal.charAt(i);
+          let reemplazo = "";
 
-        encriptedText += reemplazo;
-    }
+          switch (letra) {
+          case 'a':
+               reemplazo = "ai";
+               break;
+          case 'e':
+               reemplazo = "enter";
+               break;
+          case 'i':
+               reemplazo = "imes";
+               break;
+          case 'o':
+               reemplazo = "ober";
+               break;
+          case 'u':
+               reemplazo = "ufat";
+               break;
+          default:
+               reemplazo = letra;
+          }
 
-    document.getElementById("encriptedText").innerHTML = encriptedText;
+     textoReemplazado += reemplazo;
+     }
+
+     document.getElementById("texto-reemplazado").innerHTML = textoReemplazado;
 }
+
+function revertirReemplazo() {
+     let textoOriginal = document.getElementById("texto-original").value;
+     let textoReemplazado = "";
+
+     textoReemplazado = textoOriginal
+          .replace(/ai/g, 'a')
+          .replace(/enter/g, 'e')
+          .replace(/imes/g, 'i')
+          .replace(/ober/g, 'o')
+          .replace(/ufat/g, 'u');
+
+     document.getElementById("texto-reemplazado").innerHTML = textoReemplazado;
+     }
