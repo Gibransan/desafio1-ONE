0fb490a1a4db66e3f15da340df575b8b2c2f9e16
Gib <gibransan@outlook.com>
2023-05-28 14:52:08 -0600

    Proyecto finalizado con errores menores

--- copy.js
+++ copy.js
@@ -2,19 +2,18 @@ document.addEventListener('DOMContentLoaded', function() {
     const botonCopiar = document.getElementById('boton-copiar');
     let texto = document.getElementById('texto-reemplazado');
 
-    function copiarTexto() {
-         texto.select();
-         navigator.clipboard.writeText(texto.value)
-         .then(function() {
-              alert('¡Copiado')
-         })
-         .catch(function(error) {
-              console.error('Error al copiar: ', error);
-         });
-    }
-    botonCopiar.addEventListener('click', copiarTexto);
-
-    texto.addEventListener('input', function() {
-         texto = document.getElementById('texto-reemplazado');
-    });
+    botonCopiar.addEventListener('click', function() {
+     const textoCopiado = texto.innerText;
+     copiarTexto(textoCopiado);
+   });
+   
+   function copiarTexto(texto) {
+     navigator.clipboard.writeText(texto)
+       .then(function() {
+         alert('¡Texto copiado!');
+       })
+       .catch(function(error) {
+         console.error('Error al copiar el texto: ', error);
+       });
+   }
 })
